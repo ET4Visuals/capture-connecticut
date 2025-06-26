e165809fbeaec12a528e7f8a905516e122e15282
ET4Visuals <et4.visuals@gmail.com>
2025-06-26 13:53:48 -0400

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,10 @@
+import Map from "../components/Map";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-center mb-4">Capture Connecticut</h1>
+      <Map />
+    </div>
+  );
+}
